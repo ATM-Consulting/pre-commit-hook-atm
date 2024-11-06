8513cbf3f05f086fdd484dba7f438be9c2b33599
jpb <jean-pascal.boudet@atm-consulting>
2024-11-06 17:14:44 +0100

    remove console.log

--- check_array_start_with_t.php
+++ check_array_start_with_t.php
@@ -3,11 +3,13 @@
 
 
 function checkArrayStartWithT($filePath) {
+
+
 	// Vérifier si le fichier est un fichier PHP
 	if (!preg_match('/\.php$/', $filePath)) {
 		return; // Si ce n'est pas un fichier PHP, ne rien faire
 	}
-	console.log('enter Rule checkArrayStartWithT');
+
 	$content = file_get_contents($filePath);
 	$arrays = array_filter(explode(';', $content), function($line) {
 		return strpos($line, '[')!== false && strpos($line, ']')!== false;
